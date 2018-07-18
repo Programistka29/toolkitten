2ca9b073cdbffc6097f7714b1b2eb729cd7ee613
Dorota <porebadorota2@gmail.com>
2018-07-18 22:36:50 +0200

    Dorota Homework 1

---
+++ summer-of-code/week-01/wk1-homework-submission/soc-wk1-cert-dorota-poreba.py
@@ -0,0 +1,12 @@
+#- Hours in a year. How many hours are in a year?
+print (365 * 24)
+#for the leap year e.g 2020
+print (366 * 24)
+#- Minutes in a decade. How many minutes are in a decade?
+# Year has 365 days, day has 24 hour, hour has 60 minutes and decade equels 10 years
+print (365 * 24 * 60 * 10)
+#for the leap year e.g 2020
+print (366 * 24 * 60 * 10)
+#- Your age in seconds. How many seconds old are you?
+#age,days,hours,minutes and seconds
+print (26 * 366 * 24 * 60 * 60)
